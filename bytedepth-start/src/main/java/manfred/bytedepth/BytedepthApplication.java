3be83c190ef1021fb0d7f17d064678a91ea2466c
maxingfang <maxingfang@meituan.com>
2026-06-29 22:22:23 +0800

    feat: 新增 PostViewEventHandler 异步写入访问日志

--- bytedepth-start/src/main/java/manfred/bytedepth/BytedepthApplication.java
+++ bytedepth-start/src/main/java/manfred/bytedepth/BytedepthApplication.java
@@ -2,9 +2,11 @@ package manfred.bytedepth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = "manfred.bytedepth")
 @org.springframework.scheduling.annotation.EnableScheduling
+@EnableAsync
 public class BytedepthApplication {
     public static void main(String[] args) {
         SpringApplication.run(BytedepthApplication.class, args);
