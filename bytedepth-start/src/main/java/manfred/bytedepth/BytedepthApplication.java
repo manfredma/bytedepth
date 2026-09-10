6a434fb9c7784dbb5b51da301e4f5ef98972f64f
maxingfang <maxingfang@126.com>
2026-09-10 11:45:37 +0800

    feat: add validated network map catalog

--- bytedepth-start/src/main/java/manfred/bytedepth/BytedepthApplication.java
+++ bytedepth-start/src/main/java/manfred/bytedepth/BytedepthApplication.java
@@ -2,9 +2,11 @@ package manfred.bytedepth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = "manfred.bytedepth")
+@ConfigurationPropertiesScan
 @org.springframework.scheduling.annotation.EnableScheduling
 @EnableAsync
 public class BytedepthApplication {
